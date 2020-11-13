4ae80f7e6accf4c53a509fa01d01ce97c8f4355a
V <71629856+bombermon@users.noreply.github.com>
2020-11-13 16:51:26 +0300

    add

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 import csv, pickle
 
+temp_data = {}
+
+def save_table(data, type = 'csv'):
+    if type == 'csv':
+        with open('NewFile' + '.csv') as f:
+            data = data
+        f.close()
+
+    if type == 'pickle':
+        with open('NewFile' + '.pickle') as f:
+            data = data
+        f.close()
+
+    if type == 'txt':
+        with open('NewFile' + '.txt') as f:
+            data = data
+        f.close()
+
