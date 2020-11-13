c654a4d27a41680747e41bec165137f50f87a34c
V <71629856+bombermon@users.noreply.github.com>
2020-11-13 17:47:12 +0300

    add

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import csv, pickle
 
-temp_data = {}
+temp_data = {
+    'a': [1, 2.0, 3, 4+6j],
+    'b': ("character string", b"byte string"),
+    'c': {None, True, False}
+}
 
 def save_table(data, type = 'csv'):
     if type == 'csv':
-        with open('NewFile' + '.csv') as f:
-            data = data
+        with open('NewFile' + '.csv', mode= 'wb') as f:
+            #writer = csv.writer(f, delemiter = ',', lineterminator = '\r')
+            names = []
+            for i in data:
+                names.append(i)
+            writer = csv.DictWriter(data, delimiter=",", lineterminator="\r", fieldnames=names)
+            writer.writeheader()
+            writer.writerow(data)
+
         f.close()
 
     if type == 'pickle':
@@ -18,3 +29,4 @@ def save_table(data, type = 'csv'):
             data = data
         f.close()
 
+save_table(temp_data)
