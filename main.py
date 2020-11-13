5ffa60343a2a63e9443feaf971c13c480e978cee
V <71629856+bombermon@users.noreply.github.com>
2020-11-13 17:49:41 +0300

    add

--- main.py
+++ main.py
@@ -1,11 +1,49 @@
 import csv, pickle
 
-temp_data = {}
+temp_data = {
+    'a': [1, 2.0, 3, 4+6j],
+    'b': ("character string", b"byte string"),
+    'c': {None, True, False}
+}
+
+def load_table(file, type = "csv"):
+    dictionary = {} # Храним таблицу
+    if type == "pickle": # Считываем таблицу используя pickle
+        dictionary = pickle.load(file)
+    elif type == "csv": # Считываем таблицу используя csv
+        file_reader = csv.reader(file, delimiter=",") # преобразуем файл в лист листов
+        table_key_dictionary = {} # Создаем словарь атрибутов таблицы и записываем их номер
+        # Счетчик для подсчета количества строк
+        lines_count = 0 # Считаем номер строки
+        # Считывание данных из CSV файла
+        for row in file_reader: # Проходимся по строкам таблицы
+            if lines_count == 0: # В первой строк находяться атрибуты, создаем ключи в словаре
+                key_count = 0
+                for i in row:
+                    dictionary[i] = []
+                    table_key_dictionary[key_count] = i
+                    key_count += 1
+            else:
+                key_count = 0
+                for i in row:
+                    dictionary.get(table_key_dictionary.get(key_count)).append(i) # Записываем нужный столбик нужный элемент
+                    key_count += 1
+            lines_count += 1
+    else:
+        return -1
+    return dictionary
 
 def save_table(data, type = 'csv'):
     if type == 'csv':
-        with open('NewFile' + '.csv') as f:
-            data = data
+        with open('NewFile' + '.csv', mode= 'wb') as f:
+            #writer = csv.writer(f, delemiter = ',', lineterminator = '\r')
+            names = []
+            for i in data:
+                names.append(i)
+            writer = csv.DictWriter(data, delimiter=",", lineterminator="\r", fieldnames=names)
+            writer.writeheader()
+            writer.writerow(data)
+
         f.close()
 
     if type == 'pickle':
@@ -18,3 +56,4 @@ def save_table(data, type = 'csv'):
             data = data
         f.close()
 
+save_table(temp_data)
