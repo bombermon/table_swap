6a9bedd9540fc3b55cc62e42708f8da091309cd9
bombermon <npototun0@gmail.com>
2020-11-18 18:46:31 +0300

    добавлена функция print_table() и добавлен вывод в файл .txt

--- main.py
+++ main.py
@@ -3,10 +3,49 @@ import re
 
 
 
-temp_data = {'No': ['1','2','3','4'], 'Company': ['Ferrari','Lamba'], 'Car Model': ['488 GTB','phantom']}
+temp_data = {'No': ['1999','2','3','4'], 'Company': ['Ferrari','Lamborghini','porsche', 'BMW'],
+             'Car Model': ['488 GTB', 'phantom', 'macan', 'X5']}
+
+def print_table(data):
+    len_of_col = {}
+    max_l = 0
+    for i in data:              # ПЕРЕБОР СЛОВАРЯ ПО ВСЕМ СЛОВАМ И ЗНАЧЕНИЯМ
+        if len(i) > max_l:      # НАХОЖДЕНИЕ САМОГО ДЛИННОГО СЛОВА
+            max_l = len(i)
+        for j in data[i]:
+            if len(j) > max_l:
+                max_l = len(j)
+        len_of_col[i] = max_l
 
 
-def save_table(data, type = 'pickle'):
+    field_names_len = {}
+    for i in len_of_col:        # ФОРМИРОВАНИЕ РОВНЫХ СТОЛБЦОВ
+        temp_key = i
+        if len(i) < len_of_col[i]:
+            while len(i) < len_of_col[temp_key]:
+                i = i + ' '
+        print(i + '|', end="")
+        field_names_len[i] = len_of_col[temp_key]
+    print()
+
+
+    num_str = 0
+    while True:
+        try:
+            for i in data:                  # ДОБАВЛЕНИЕ ПРОБЕЛОВ ДЛЯ РОВНЫХ СТОЛБЦОВ
+                temp = data[i][num_str]
+                if len(temp) < len_of_col[i]:
+                    while len(temp) < len_of_col[i]:
+                        temp += ' '
+                temp += '|'
+                print(temp, end='')
+            print()
+            num_str += 1
+        except IndexError:
+            break
+
+
+def save_table(data, type = 'txt'):
     if type == 'csv':
         temp_table = []
         field_names = data.keys()
@@ -31,11 +70,42 @@ def save_table(data, type = 'pickle'):
         f.close()
 
     elif type == 'txt':
-        with open('NewFile' + '.txt') as f:
-            data = data
-        f.close()
+        with open('NewFile' + '.txt', 'w') as f:
+            len_of_col = {}
+            max_l = 0
+            for i in data:  # ПЕРЕБОР СЛОВАРЯ ПО ВСЕМ СЛОВАМ И ЗНАЧЕНИЯМ
+                if len(i) > max_l:  # НАХОЖДЕНИЕ САМОГО ДЛИННОГО СЛОВА
+                    max_l = len(i)
+                for j in data[i]:
+                    if len(j) > max_l:
+                        max_l = len(j)
+                len_of_col[i] = max_l
 
-save_table(temp_data, type='pickle')
+            field_names_len = {}
+            for i in len_of_col:  # ФОРМИРОВАНИЕ РОВНЫХ СТОЛБЦОВ
+                temp_key = i
+                if len(i) < len_of_col[i]:
+                    while len(i) < len_of_col[temp_key]:
+                        i = i + ' '
+                print(i + '|', file=f, end="")
+                field_names_len[i] = len_of_col[temp_key]
+            print(file=f)
+
+            num_str = 0
+            while True:
+                try:
+                    for i in data:  # ДОБАВЛЕНИЕ ПРОБЕЛОВ ДЛЯ РОВНЫХ СТОЛБЦОВ
+                        temp = data[i][num_str]
+                        if len(temp) < len_of_col[i]:
+                            while len(temp) < len_of_col[i]:
+                                temp += ' '
+                        temp += '|'
+                        print(temp, file=f, end='')
+                    print(file=f)
+                    num_str += 1
+                except IndexError:
+                    break
+        f.close()
 
 
 def load_table(file, type = "csv"):
@@ -64,3 +134,8 @@ def load_table(file, type = "csv"):
     else:
         return -1
     return dictionary
+
+
+
+
+save_table(temp_data)
--- newfile.py
+++ newfile.py
@@ -1,45 +1,38 @@
-temp_data = {'No': ['1999','2','3','4'], 'Company': ['Ferrari','Lamba'], 'Car Model': ['488 GTB','phantom']}
+data = {'No': ['1999','2','3','4'], 'Company': ['Ferrari','Lamborghini','porsche', 'BMW'],
+             'Car Model': ['488 GTB', 'phantom', 'macan', 'X5']}
 
-def print_table(data):
+with open('NewFile.txt', 'w') as f:
     len_of_col = {}
     max_l = 0
-    for i in data:              # ПЕРЕБОР СЛОВАРЯ ПО ВСЕМ СЛОВАМ И ЗНАЧЕНИЯМ
-        if len(i) > max_l:      # НАХОЖДЕНИЕ САМОГО ДЛИННОГО СЛОВА
+    for i in data:  # ПЕРЕБОР СЛОВАРЯ ПО ВСЕМ СЛОВАМ И ЗНАЧЕНИЯМ
+        if len(i) > max_l:  # НАХОЖДЕНИЕ САМОГО ДЛИННОГО СЛОВА
             max_l = len(i)
         for j in data[i]:
             if len(j) > max_l:
                 max_l = len(j)
         len_of_col[i] = max_l
 
-    print(len_of_col)
-
     field_names_len = {}
-    for i in len_of_col:        # ФОРМИРОВАНИЕ РОВНЫХ СТОЛБЦОВ
+    for i in len_of_col:  # ФОРМИРОВАНИЕ РОВНЫХ СТОЛБЦОВ
         temp_key = i
         if len(i) < len_of_col[i]:
             while len(i) < len_of_col[temp_key]:
                 i = i + ' '
-        print(i + '|', end="")
+        print(i + '|', file=f, end="")
         field_names_len[i] = len_of_col[temp_key]
-    print()
-
+    print(file=f)
 
     num_str = 0
     while True:
         try:
-            for i in data:
+            for i in data:  # ДОБАВЛЕНИЕ ПРОБЕЛОВ ДЛЯ РОВНЫХ СТОЛБЦОВ
                 temp = data[i][num_str]
                 if len(temp) < len_of_col[i]:
                     while len(temp) < len_of_col[i]:
                         temp += ' '
                 temp += '|'
-                print(temp)
-                num_str+=1
+                print(temp, file=f, end='')
+            print(file=f)
+            num_str += 1
         except IndexError:
             break
-
-
-
-
-
-print_table(temp_data)
