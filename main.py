671f565c3591d042547db1549a0676833d447dce
bombermon <npototun0@gmail.com>
2020-11-21 14:29:59 +0300

    added set_values()

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import csv, pickle
 import re
 
 temp_data = [{'No': ['1999', '2', '3', '4'], 'Company': ['Ferrari', 'Lamborghini', 'porsche', 'BMW'],
-              'Car Model': ['488 GTB', 'phantom', 'macan', 'X5']}, {'No': [float, bool, str], 'Company': [float, int, bool, str],
+              'Car Model': ['488 GTB', 'phantom', 'macan', 'X5']},
+             {'No': [float, int, bool, str], 'Company': [float, int, bool, str],
               'Car Model': [float, int, bool, str]}]
 
-temp_spisok = ['1', '2', 3, '4', '5']
+temp_spisok = ['1', '2', '3', '4', '5']
 
 
 def print_table():
@@ -16,7 +17,7 @@ def print_table():
         if len(i) > max_l:  # НАХОЖДЕНИЕ САМОГО ДЛИННОГО СЛОВА
             max_l = len(i)
         for j in data[i]:
-            if len(str(j)) > max_l:
+            if len(j) > max_l:
                 max_l = len(j)
         len_of_col[i] = max_l
 
@@ -34,9 +35,9 @@ def print_table():
     while True:
         try:
             for i in data:  # ДОБАВЛЕНИЕ ПРОБЕЛОВ ДЛЯ РОВНЫХ СТОЛБЦОВ
-                temp = str(data[i][num_str])
-                if len(str(temp)) < len_of_col[i]:
-                    while len(str(temp)) < len_of_col[i]:
+                temp = data[i][num_str]
+                if len(temp) < len_of_col[i]:
+                    while len(temp) < len_of_col[i]:
                         temp += ' '
                 temp += '|'
                 print(temp, end='')
@@ -59,6 +60,7 @@ def save_table(data, type='csv'):
                 if i < len(data[j]):
                     values[j] = data[j][i]
             temp_table.append(values)
+        print(temp_table)
         with open('NewFile' + '.csv', 'w') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=field_names)
             writer.writeheader()
@@ -67,7 +69,6 @@ def save_table(data, type='csv'):
     elif type == 'pickle':
         with open('NewFile' + '.pickle', 'wb') as f:
             pickle.dump(data, f)
-        f.close()
 
     elif type == 'txt':
         with open('NewFile' + '.txt', 'w') as f:
@@ -106,7 +107,6 @@ def save_table(data, type='csv'):
                     num_str += 1
                 except IndexError:
                     break
-        f.close()
 
 
 def load_table(file, type="csv"):
@@ -149,14 +149,13 @@ def set_values(values, column=0):
         for i in data:
             field_names.append(i)
         for k in values:
+            flag = False
             for j in typer[field_names[column]]:
                 if type(k) == j:
                     flag = True
-
-        if flag == False:
-            print('Оишбка! Вы ввели значения, не подходящие данному столбцу(', typer[field_names[column]], ')')
-            return
-
+            if flag == False:
+                print('Оишбка! Вы ввели значения, не подходящие данному столбцу(', typer[field_names[column]], ')')
+                return
 
         data[field_names[column]] = values
     except IndexError:
@@ -167,7 +166,7 @@ def set_values(values, column=0):
 
 set_values(temp_spisok)
 
-save_table(temp_data[0])
+save_table(temp_data[0], type='csv')
 print_table()
 
 # ЗОНА ТЕСТОВ КОНЕЦ ЗОНА ТЕСТОВ КОНЕЦ ЗОНА ТЕСТОВ КОНЕЦ ЗОНА ТЕСТОВ КОНЕЦ ЗОНА ТЕСТОВ КОНЕЦ ЗОНА ТЕСТОВ КОНЕЦ ЗОНА
